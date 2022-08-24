f6e546fd9f98fb060867760e3add6e361d455d94
Ritik <ritikkhandelwal650@gmail.com>
2022-08-23 22:05:58 -0700

    four code

---
+++ sort in descending order .js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers as argument
+// It should return an array with the numbers sorted in descending order
+
+
+function myFunction(arr) {
+   
+    return(arr.sort().reverse())
+    myFunction([3,2,1])
+}
