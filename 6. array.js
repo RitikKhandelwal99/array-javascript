c02cc50f0d0662f9cb1a810f9e7f528b12e02b48
Ritik <ritikkhandelwal650@gmail.com>
2022-09-05 21:45:18 -0700

    array

---
+++ 6. array.js
@@ -0,0 +1,30 @@
+// create a  array 
+// three types of array 
+
+//const arr = [1,2,3];
+//console.log(arr);
+
+// create a array using function 
+function myFunction() {
+    console.log(myFunction);
+}
+const arr = [1];
+const arr1 = [2];
+const arr2 = [3];
+
+const result = [arr,arr1,arr2];
+console.log(result);
+
+// json array 
+const obj = {
+    "a" : 'rahul',
+    "b" :  12,
+};
+console.log(obj);
+
+// 1. string type to create a array 
+//2. boolean array.
+//3. mixed array 
+
+const x = [1, true, 2]; // mixed array 
+console.log(x);
